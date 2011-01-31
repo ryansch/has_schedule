9e0687cd730c184f62f58a4ebf98be46d087a296
Ryan Schlesinger <ryan@instanceinc.com>
2011-01-31 15:13:59 -0800

    Adding has_schedule.

---
+++ lib/has_schedule.rb
@@ -0,0 +1,38 @@
+module HasSchedule
+	def self.included(base)
+		base.send :extend, ClassMethods
+	end
+
+	module ClassMethods
+		def has_schedule
+			send :include, InstanceMethods
+
+			attr_writer :schedule
+
+			alias_method_chain :reload, :schedule
+			alias_method_chain :save, :schedule
+		end
+	end
+
+	module InstanceMethods
+		def reload_with_schedule
+			@schedule = nil
+			reload_without_schedule
+		end
+		
+		def save_with_schedule
+			self.schedule_yaml = @schedule.to_yaml unless @schedule.nil?
+			save_without_schedule
+		end
+
+		def schedule
+			@schedule ||= if self.schedule_yaml.blank? 
+											nil
+										else
+											IceCube::Schedule.from_yaml(self.schedule_yaml)
+										end
+		end
+	end
+end
+
+ActiveRecord::Base.send :include, HasSchedule
