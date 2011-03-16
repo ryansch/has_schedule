4ad25c6b66503d6276033e917abdf300ab414cf8
Ryan Schlesinger <ryan@instanceinc.com>
2011-03-16 09:42:10 -0700

    Fixing accidental clearing of schedule.

--- lib/has_schedule.rb
+++ lib/has_schedule.rb
@@ -15,18 +15,24 @@ module HasSchedule
 	module InstanceMethods
 		def reload_with_schedule
 			@schedule = nil
+			@nil_requested = nil
 			reload_without_schedule
 		end
 		
 		def save_with_schedule
-			self.schedule_yaml = @schedule.andand.to_yaml
+			if (@schedule.nil? and @nil_requested != nil) or @schedule != nil
+				self.schedule_yaml = @schedule.andand.to_yaml
+			end
 		end
 
 		def schedule=(schedule)
 			@schedule = schedule
 
 			if @schedule.nil?
+				@nil_requested = true
 				save_with_schedule
+			else
+				@nil_requested = nil
 			end
 		end
 
